7a10d1ce32b1dd8096502c9189cf354bd766986f
fkrause98 <fkrausear@gmail.com>
2025-03-16 10:03:50 -0300

    finish: triggers persist rework

---
+++ lib/dankie/store/triggers.ex
@@ -0,0 +1,54 @@
+defmodule Dankie.Store.Triggers do
+  require Logger
+  @triggers_table_prefix "chat_trigger_table"
+
+  defp table_path_for_chat_id(chat_id),
+    do: ~c"./store/#{@triggers_table_prefix}_#{chat_id}"
+
+  defp table_name_for_chat_id(chat_id),
+    do: ~c"#{@triggers_table_prefix}_#{chat_id}"
+
+  defp open_chat_table(chat_id) do
+    table_path =
+      chat_id
+      |> table_path_for_chat_id
+
+    table_name =
+      chat_id
+      |> table_name_for_chat_id
+
+    :dets.open_file(table_name, file: table_path)
+  end
+
+  @spec store_trigger(Regex.t(), integer(), integer()) :: :ok | {:error, term()}
+  def store_trigger(new_trigger, chat_id, msg_id) do
+    with {:ok, table_name} <- open_chat_table(chat_id),
+         :ok <- :dets.insert(table_name, {new_trigger, {chat_id, msg_id}}),
+         :ok <- :dets.close(table_name) do
+      :ok
+    else
+      err ->
+        Logger.error(
+          "Could not add trigger! Got error: #{inspect(err)}, parameters: #{new_trigger}, #{chat_id}, #{msg_id}"
+        )
+
+        err
+    end
+  end
+
+  @spec traverse_triggers_table(integer(), function()) :: :ok | {:error, term()}
+  def traverse_triggers_table(chat_id, traverse_fun) when is_function(traverse_fun) do
+    with {:ok, table_name} <- open_chat_table(chat_id),
+         traverse_result <- :dets.traverse(table_name, traverse_fun),
+         :ok <- :dets.close(table_name) do
+      {:ok, traverse_result}
+    else
+      err ->
+        Logger.error(
+          "Error while traversing trigger table: #{inspect(err)}, for chat id: #{chat_id}"
+        )
+
+        err
+    end
+  end
+end
