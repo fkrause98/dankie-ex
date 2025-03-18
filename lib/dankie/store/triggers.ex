a8965c6b652a947bed17dee72b634d7d44a10efe
fkrause98 <fkrausear@gmail.com>
2025-03-17 22:51:18 -0300

    fix: db path fix name

--- lib/dankie/store/triggers.ex
+++ lib/dankie/store/triggers.ex
@@ -1,6 +1,6 @@
 defmodule Dankie.Store.Triggers do
   require Logger
-  @triggers_table_prefix "chat_trigger_table"
+  @triggers_table_prefix "./store/#{Mix.env()}/chat_triggers"
 
   @moduledoc """
   Module to interface between triggers logic and RocksDB storage.
@@ -10,7 +10,7 @@ defmodule Dankie.Store.Triggers do
 
   @spec db_path_for_chat_id(integer()) :: String.t()
   defp db_path_for_chat_id(chat_id),
-    do: "./store/#{Mix.env()}/#{@triggers_table_prefix}_#{chat_id}"
+    do: @triggers_table_prefix <> "_#{chat_id}"
 
   defp db_options do
     [
